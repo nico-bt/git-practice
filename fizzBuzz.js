6fba25c839ff8e57aa0de4224d5090dcde220b2c
Nico-Battaglia <nico-battaglia@hotmail.com>
2022-07-16 22:21:12 -0300

    Add bye message

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -3,3 +3,6 @@ const range = [...Array(100).keys()].map(x => x+1);
 range.map(item =>{
     console.log(item % 5==0 ? "Multiple of 5" : item)  
 })
+
+console.log("\---------------------")
+console.log("\nEnd of program. Bye")
